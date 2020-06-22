a4e94372481e1f28d3067a72b112f084b976ce72
Matt <matt.teach@brooklynfrontiers.org>
2020-06-22 11:41:27 -0400

    memo here describing recent changes

--- app.py
+++ app.py
@@ -3,15 +3,23 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-import model
+from model import determineStone
 
 
 # -- Initialization section --
 app = Flask(__name__)
-
 
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    name = request.form['name']
+    month = request.form['month']
+    gem = determineStone(month)
+    if request.method == 'GET':
+        return "ERROR!"
+    else:
+        return render_template('results.html', name = name, month=month, gem=gem)
---
+++ model.py
@@ -0,0 +1,23 @@
+def determineStone(month):
+    if month.lower() == "january": 
+        return "Garnet"
+    elif month.lower() =="february": 
+        return "Amethyst"
+    elif month.lower() =="march": 
+        return "Aquamarine & Bloodstone"
+    elif month.lower() =="april":
+        return "Diamond"
+    elif month.lower()=="may":
+        return "Emerald"
+    elif month.lower()=="June": 
+        return "Pearl, Moonstone, Alexandrite"
+    elif month.lower()=="July":
+        return "Ruby"
+    else: 
+        return "I'm not sure!"
+
+# August– Peridot, Spinel
+# September– Sapphire
+# October– Opal, Tourmaline
+# November– Topaz, Citrine
+# December– Turquoise, Zircon, Tanzanite
